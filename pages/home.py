272b66301c19d7896f09f26b1b2bf3ccdfc45fe0
nicktriplett <124808763+nicktriplett@users.noreply.github.com>
2023-08-28 01:33:41 -0500

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,9 +1,12 @@
 # Dashboard Home Page
 
+import os
+import pathlib
+from pathlib import Path
 import pandas as pd
 import plotly.express as px
 import dash
-from dash import Dash, html, dcc, Input, Output, State
+from dash import Dash, html, dcc, Input, Output, dash_table
 import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path='/')
